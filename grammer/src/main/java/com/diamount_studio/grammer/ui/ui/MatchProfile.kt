3f0ea3f29007d6465ebea53edc621f1434479a9f
MissDaizy <dianaukrainsky@gmail.com>
2023-04-23 19:10:42 +0300

    Started implementing the list of profiles in each class.

---
+++ grammer/src/main/java/com/diamount_studio/grammer/ui/ui/MatchProfile.kt
@@ -0,0 +1,21 @@
+package com.diamount_studio.grammer.ui.ui
+
+import com.diamount_studio.common.R
+import com.diamount_studio.common.features.tinder_cards.presentation.MatchProfile
+
+fun MatchProfile() {
+    val profiles = listOf(
+        MatchProfile("Preserved", R.drawable.gif_preserved),
+        MatchProfile("Fun", R.drawable.gif_having_fun),
+        MatchProfile("Mortgages", R.drawable.gif_mortgages),
+        MatchProfile("Bulb", R.drawable.gif_bulb),
+        MatchProfile("Chatter", R.drawable.gif_chatter),
+        MatchProfile("Cosmic", R.drawable.gif_cosmic),
+        MatchProfile("Frugal", R.drawable.gif_frugal),
+        MatchProfile("Invest", R.drawable.gif_invest),
+        MatchProfile("Expand", R.drawable.gif_expand),
+        MatchProfile("Lean", R.drawable.gif_lean),
+//    MatchProfile("Fun", R.drawable.ic_preserved),
+//    MatchProfile("Fun", R.drawable.ic_having_fun),
+    )
+}
